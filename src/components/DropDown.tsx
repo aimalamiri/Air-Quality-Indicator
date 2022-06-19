eb4a0f6abd8cc79d35db777a201c59774586ede1
Aimal Amiri <meaimal@hotmail.com>
2022-06-19 12:27:18 +0430

    Fix the DropDownComponent issues

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -3,12 +3,24 @@ import PropTypes from 'prop-types'
 
 interface Props {
   name: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
   options: Array<string>
 }
 
-const DropDown: React.FC<Props> = ({ name, options }): ReactElement => {
+const DropDown: React.FC<Props> = ({ name, value, onChange, options }): ReactElement => {
+  const [selected, setSelected] = React.useState<string>(value)
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): string => {
+    onChange(event);
+    setSelected(event.target.value)
+    return selected
+  }
+
   return (
-    <select name={name}>
+    <select value={selected} name={name} onChange={(e) => handleChange(e)}>
       {options.map((option: string, i: number) => (
         <option key={i} value={option}>
           {option}
@@ -20,6 +32,7 @@ const DropDown: React.FC<Props> = ({ name, options }): ReactElement => {
 
 DropDown.propTypes = {
   name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   options: PropTypes.array.isRequired,
 }
 
